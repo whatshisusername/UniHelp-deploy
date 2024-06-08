a2d94ce20b30ae67e57fbbd73805fb9a1e151de0
whatshisusername <nk0775536@gmail.com>
2024-06-08 23:43:51 +0530

    timed out

--- src/index.js
+++ src/index.js
@@ -9,31 +9,31 @@ dotenv.config({
 });
 
 // running our mongodb connect function declared in db/index.js used to connect to mongodatabase
-// connectDB()
-// .then(
-    
-//     app.listen(process.env.PORT||8000,()=>
-//     {
-//         console.log(`server is running on port ${process.env.PORT}`);
-//     })
-    
-// )
-// .catch((error)=>{
-//     console.log("MONGODB connection failed",error);
-// })
-
 connectDB()
-    .then(() => {
-        const server = app.listen(process.env.PORT || 8000, () => {
-            console.log(`Server is running on port ${process.env.PORT || 8000}`);
-        });
-
-        // Set the server timeout to 2 minutes (120000 milliseconds)
-        server.setTimeout(120000);
+.then(
+    
+    app.listen(process.env.PORT||8000,()=>
+    {
+        console.log(`server is running on port ${process.env.PORT}`);
     })
-    .catch((error) => {
-        console.log("MONGODB connection failed", error);
-    });
+    
+)
+.catch((error)=>{
+    console.log("MONGODB connection failed",error);
+})
+
+// connectDB()
+//     .then(() => {
+//         const server = app.listen(process.env.PORT || 8000, () => {
+//             console.log(`Server is running on port ${process.env.PORT || 8000}`);
+//         });
+
+//         // Set the server timeout to 2 minutes (120000 milliseconds)
+//         server.setTimeout(120000);
+//     })
+//     .catch((error) => {
+//         console.log("MONGODB connection failed", error);
+//     });
 
 
 
