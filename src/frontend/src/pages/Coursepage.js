56a7e4c8a9033d1f1f131638840f397bd2d1e3a4
whatshisusername <nk0775536@gmail.com>
2024-06-08 17:35:25 +0530

    notes download

--- src/frontend/src/pages/Coursepage.js
+++ src/frontend/src/pages/Coursepage.js
@@ -666,7 +666,8 @@ export default function PostCard() {
                         {notes.map((note) => (
                             <li key={note._id} className="flex justify-between items-center py-2 border-b">
                                 <div>
-                                    <p className="text-lg">{note.filename}-<a href={note.notesfile} target="_blank" className="text-blue-500">{note.notesfile}</a></p>
+                                    <p className="text-lg">{note.filename}-<a href={note.notesfile} target="_blank" className="bg-grey-light hover:bg-grey text-grey-darkest font-bold py-2 px-4 rounded inline-flex items-center"><svg className="w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/></svg>
+                                    <span>Download</span></a></p>
                                 </div>
                                 
                             </li>
