9c107faf50381a03c3d3bd8f0a89b738ec9a6eca
whatshisusername <nk0775536@gmail.com>
2024-06-12 00:58:47 +0530

    add event generate description from Ai

--- src/frontend/src/pages/AddEvent.js
+++ src/frontend/src/pages/AddEvent.js
@@ -83,6 +83,31 @@ function AddEvent() {
     }
     setresponse('')
   };
+  const [generatingAnswer, setGeneratingAnswer] = useState(false);
+
+  async function generatedescription() {
+    setGeneratingAnswer(true);
+    setDescription("Loading your description \n It might take upto 10 seconds");
+    try {
+      const response = await axios({
+        url: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyCpzGSyTEUtip5KocQjsc4eZsDlyND6oP4"
+         ,
+        method: "post",
+        data: {
+          contents: [{ parts: [{ text: `write description for event ${title} in 4 lines only just description no other details` }] }],
+        },
+      });
+
+      setDescription(
+        response["data"]["candidates"][0]["content"]["parts"][0]["text"]
+      );
+    } catch (error) {
+      console.log(error);
+      setDescription("Sorry - Something went wrong. Please try again or Add your own description!");
+    }
+
+    setGeneratingAnswer(false);
+  }
 
 
   return (
@@ -124,6 +149,20 @@ function AddEvent() {
           value={title}
           onChange={(e) => setTitle(e.target.value)}
         />
+        {/* <button onClick={generatedescription}>Generate Description from AI</button> */}
+        <button onClick={generatedescription} 
+	class="group relative overflow-hidden bg-green-600 focus:ring-4 focus:ring-green-300 inline-flex items-center px-7 py-2.5 rounded-lg text-white justify-center">
+	<span class="z-40">Generate Description from AI</span>
+	<svg class="z-40 ml-2 -mr-1 w-3 h-3 transition-all duration-300 group-hover:translate-x-1" fill="currentColor"
+		viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+		<path fill-rule="evenodd"
+			d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+			clip-rule="evenodd"></path>
+	</svg>
+	<div
+		class="absolute inset-0 h-[200%] w-[200%] rotate-45 translate-x-[-70%] transition-all group-hover:scale-100 bg-white/30 group-hover:translate-x-[50%] z-20 duration-1000">
+	</div>
+</button>
         <label >Description</label>
         <textarea
           className="description bg-gray-100 sec p-3 h-60 border border-gray-300 outline-none"
